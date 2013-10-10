43c72e3d60ded95b61581e9d33e1b7460bdf8999
Tejas <tgujarathi@equalexpets.com>
2013-10-10 12:53:49 +0100

    added code to prepare n pdfs with n sheets

--- src/main/java/com/ee/excellentpdf/XlsxFileReader.java
+++ src/main/java/com/ee/excellentpdf/XlsxFileReader.java
@@ -20,51 +20,62 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 public class XlsxFileReader {
 
-	public static void main(String[] args) throws DocumentException, IOException {
+	public static void main(String[] args) throws DocumentException,
+			IOException {
 		// First we read the Excel file in binary format into FileInputStream
 		FileInputStream input_document = new FileInputStream(new File(
 				"ExcelFile/xl1.xls"));
 		// Read workbook into HSSFWorkbook
 		HSSFWorkbook my_xls_workbook = new HSSFWorkbook(input_document);
 		// Read worksheet into HSSFSheet
-		HSSFSheet my_worksheet = my_xls_workbook.getSheetAt(0);
-		// To iterate over the rows
-		Iterator<Row> rowIterator = my_worksheet.iterator();
-		// We will create output PDF document objects at this point
-		Document iText_xls_2_pdf = new Document();
-		PdfWriter.getInstance(iText_xls_2_pdf, new FileOutputStream(
-				"PDFs/Excel2PDF_Output.pdf"));
-		iText_xls_2_pdf.open();
-		// we have two columns in the Excel sheet, so we create a PDF table with
-		// two columns
-		// Note: There are ways to make this dynamic in nature, if you want to.
-		PdfPTable my_table = new PdfPTable(2);
-		// We will use the object below to dynamically add new data to the table
-		PdfPCell table_cell;
-		// Loop through rows.
-		while (rowIterator.hasNext()) {
-			Row row = rowIterator.next();
-			Iterator<Cell> cellIterator = row.cellIterator();
-			while (cellIterator.hasNext()) {
-				Cell cell = cellIterator.next(); // Fetch CELL
-				switch (cell.getCellType()) { // Identify CELL type
-				// you need to add more code here based on
-				// your requirement / transformations
-				case Cell.CELL_TYPE_STRING:
-					// Push the data from Excel to PDF Cell
-					table_cell = new PdfPCell(new Phrase(
-							cell.getStringCellValue()));
-					// feel free to move the code below to suit to your needs
-					my_table.addCell(table_cell);
-					break;
+		
+		int i = 0;
+		while (i < my_xls_workbook.getNumberOfSheets()) {
+			HSSFSheet my_worksheet = my_xls_workbook.getSheetAt(i);
+			// To iterate over the rows
+			Iterator<Row> rowIterator = my_worksheet.iterator();
+			// We will create output PDF document objects at this point
+			Document iText_xls_2_pdf = new Document();
+			PdfWriter.getInstance(iText_xls_2_pdf, new FileOutputStream(
+					"PDFs/Excel2PDF_Output"+i+".pdf"));
+			iText_xls_2_pdf.open();
+			// we have two columns in the Excel sheet, so we create a PDF table
+			// with
+			// two columns
+			// Note: There are ways to make this dynamic in nature, if you want
+			// to.
+			PdfPTable my_table = new PdfPTable(2);
+			// We will use the object below to dynamically add new data to the
+			// table
+			PdfPCell table_cell;
+			// Loop through rows.
+			while (rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next(); // Fetch CELL
+					switch (cell.getCellType()) { // Identify CELL type
+					// you need to add more code here based on
+					// your requirement / transformations
+					case Cell.CELL_TYPE_STRING:
+						// Push the data from Excel to PDF Cell
+						table_cell = new PdfPCell(new Phrase(
+								cell.getStringCellValue()));
+						// feel free to move the code below to suit to your
+						// needs
+						my_table.addCell(table_cell);
+						break;
+					}
+					// next line
 				}
-				// next line
-			}
 
+			}
+			// Finally add the table to PDF document
+			iText_xls_2_pdf.add(my_table);
+			iText_xls_2_pdf.close();
+			i++;
 		}
-		// Finally add the table to PDF document
-		iText_xls_2_pdf.add(my_table);
-		iText_xls_2_pdf.close();
+		System.out.println("Done!");
 		// we created our pdf file..
 		input_document.close(); // close xls
 	}
