ec0cff93a94a4dc6f968c5e7c752c381aa45f570
Mrugen Deshmukh <mrugenmike@gmail.com>
2013-10-11 11:04:15 +0530

    chnaged mvc config to XML based config.

--- src/main/java/com/ee/excellentpdf/web/HomeController.java
+++ src/main/java/com/ee/excellentpdf/web/HomeController.java
@@ -2,15 +2,22 @@ package com.ee.excellentpdf.web;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Component
 @Controller
 public class HomeController {
 
-  @RequestMapping("/home")
-    public String goHome(){
-      return "redirect:home";
+  @RequestMapping("/")
+    public String goHome() throws IOException {
+      return "home";
   }
+
+    @RequestMapping("/upload")
+    public String convertToPDf(MultipartFile file){
+        return "home";
+    }
 }
---
+++ src/test/java/com/ee/excellentpdf/web/HomeControllerTest.java
@@ -0,0 +1,11 @@
+package com.ee.excellentpdf.web;
+
+import com.ee.excellentpdf.reader.ExcelService;
+import org.junit.Test;
+
+public class HomeControllerTest {
+
+    @Test
+    public void itShouldAcceptExcelForConversion(){
+    }
+}
